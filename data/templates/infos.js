e430f94acc7371efff3a6e55fadbec18e94f2172
Jean-Michel Simard <jeanm.simard@gmail.com>
2021-11-09 17:33:22 -0500

    WIP Got rid of table for infos billboard

--- data/templates/infos.js
+++ data/templates/infos.js
@@ -1,66 +1,55 @@
-export const template = `<div data-mapping data-hover-class="test" onclick="javascript:alert(123)">
-{{it.fullName}}
-</div>`;
-
-export const template2 = `<table>
-            <thead>
-                <th colspan="3" data-mapping data-hover-class="test" onclick="javascript:alert(123)">{{it.fullName}}</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td class="font-bold" >Vocabulaire (nom français de la composante):</td>
-                    <td class="bg-gray-600" colspan="2">{{it.vocableFR}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold">Vocabulaire (vocabulaire usuel de la composante):</td>
-                    <td class="bg-gray-600" colspan="2">{{it.vocableEN}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold">Matériaux:</td>
-                    <td class="bg-gray-600">{{it.materials.name}}</td>
-                    <td class="bg-gray-600">{{it.materials.description}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold">Dimensions standards:</td>
-                    <td class="bg-gray-600" colspan="2">Hauteur {{it.standradSizes.height}}<br>Largeur
-                        {{it.standradSizes.width}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold">Dimensions possibles: limite inférieur</td>
-                    <td class="bg-gray-600">Hauteur : <br>{{it.possibleSizes.height}}</td>
-                    <td class="bg-gray-600">Largeur : <br>{{it.possibleSizes.width}}</td>
-                </tr>
-                <tr>
-                    <td class="font-bold">Lien vers des images de constructions réelles (en différents contextes)</td>
-                    <td class="bg-gray-600" colspan="2">
-                    <img src="./data/images/semelle.jpg" alt=""><img src="./data/images/semelle2.jpg" alt=""><img src="./data/images/semelle3.jpg" alt="">
-                    </td>
-                </tr>
-                <tr>
-                    <td class="font-bold">Norme du Code de construction du Québec:</td>
-                    <td class="bg-gray-600">Extrait du Code construction du Québec <br>voir plus bas</td>
-                    <td class="bg-gray-600">{{it.code.article}}
+export const template = `
+<div class="head" data-mapping data-hover-class='test'>{{it.fullName}}</div>
+  <div class="row">
+    <div class="cell" data-mapping data-hover-class='cell-hover'>Vocabulaire (nom français de la composante):</div>
+    <div class="cell">{{it.vocableFR}}</div>
+  </div>
+  <div class="row">
+    <div class="cell">Vocabulaire (nom usuel):</div>
+    <div class="cell">{{it.vocableEN}}</div>
+  </div>
+  <div class="row">
+    <div class="cell">Matériaux:</div>
+    <div class="cell">{{it.materials.name}}</div>
+    <div class="cell">{{it.materials.description}}</div>
+  </div>
+   <div class="row">
+    <div class="cell">Dimensions standards:</div>
+    <div class="cell">Hauteur {{it.standradSizes.height}}
+      <br>Largeur {{it.standradSizes.width}}
+     </div>
+  </div>
+  <div class="row">
+    <div class="cell">Dimensions possibles: limite inférieure</div>
+    <div class="cell">Hauteur : <br>{{it.possibleSizes.height}}</div>
+    <div class="cell">Largeur : <br>{{it.possibleSizes.width}}</div>
+  </div>
+  
+  <div class="row">
+    <div class="cell">Images en différents contextes</div>
+    <div class="cell flex"><img src="./data/images/semelle.jpg" alt=""><img src="./data/images/semelle2.jpg" alt=""><img src="./data/images/semelle3.jpg" alt=""></div>
+  </div>
+  
+  <div class="row">
+    <div class="cell">Normes du Code de construction du Québec:</div>
+    <div class="cell">Extrait du Code construction du Québec <br>voir plus bas</div>
+    <div class="cell">
+      {{it.code.article}}
                         <br>{{it.code.links[0].name}}
                         <br> <a href="{{it.code.links[0].url}}">{{it.code.links[0].url}}</a>
                         <br> et
                         <br>{{it.code.links[1].name}}
-                        <br> <a href="{{it.code.links[1].url}}">{{it.code.links[1].url}}</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td class="font-bold">Information textuelle sur les prescriptions de pose et /ou techniques
-                        d’assemblages
-                    </td>
-                    <td class="bg-gray-600" colspan="2">{{it.description}}</td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="3"> <button class="bg-gray-500 p-5" onclick="javascript:alert(123)">button</button>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>`;
-
-
-
+                        <br> <a href="{{it.code.links[1].url}}">{{it.code.links[1].url}}</a></div>
+  </div>
+  
+  <div class="row">
+    <div class="cell">Information textuelle sur les prescriptions de pose et /ou techniques d’assemblages</div>
+    <div class="cell">{{it.description}}</div>
+  </div>
+  
+  <div class="row">
+    <div class="cell">
+    <button data-mapping class='button' data-hover-class='button-hover' data-down-class='button-down'  onclick="javascript:alert(123)">button</button>
+    </div>
+  </div>
+`;
