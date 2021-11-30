31004a6c6bfe1bf50e9a07933beaa572e2ca61c0
Philip Taylor <phil@zea.live>
2021-11-30 14:31:57 -0500

    chore: commit prettier.yaml

--- src/loadLabels.js
+++ src/loadLabels.js
@@ -1,26 +1,24 @@
-const { resourceLoader } = window.zeaEngine;
+const { resourceLoader } = window.zeaEngine
 
 const loadLabels = (url) => {
   return new Promise((resolve) => {
-    resourceLoader.loadFile("binary", url).then((arrayBuffer) => {
-      const unit8array = new Uint8Array(arrayBuffer);
+    resourceLoader.loadFile('binary', url).then((arrayBuffer) => {
+      const unit8array = new Uint8Array(arrayBuffer)
       const workbook = XLSX.read(unit8array, {
-        type: "array",
-      });
-      const json = {};
+        type: 'array',
+      })
+      const json = {}
       workbook.SheetNames.forEach(function (sheetName) {
         // Here is your object
-        const rows = XLSX.utils.sheet_to_row_object_array(
-          workbook.Sheets[sheetName]
-        );
+        const rows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName])
         rows.forEach(function (row) {
-          const identifier = row.Identifier;
-          delete row.Identifier;
-          json[identifier] = row;
-        });
-        resolve(json);
-      });
-    });
-  });
-};
-export default loadLabels;
+          const identifier = row.Identifier
+          delete row.Identifier
+          json[identifier] = row
+        })
+        resolve(json)
+      })
+    })
+  })
+}
+export default loadLabels
