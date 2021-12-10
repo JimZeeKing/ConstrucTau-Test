ed0a1dc6582507a11af9e3c58a0f458bf162d32a
Jean-Michel Simard <jeanm.simard@gmail.com>
2021-12-09 23:37:41 -0500

    FEAT Tree , paging in controls

--- src/left-controls.js
+++ src/left-controls.js
@@ -2,6 +2,9 @@ const LeftControls = {
   data() {
     return {
       closed: true,
+      currentPage: 0,
+      pages: [{ title: 'Éléments' }, { title: 'Positions' }],
+      positions: [],
       tree: {
         name: '',
         geomitems: [],
@@ -10,99 +13,74 @@ const LeftControls = {
   },
 }
 
-const leftControls = Vue.createApp(LeftControls);
-
+const leftControls = Vue.createApp(LeftControls)
 
 leftControls.component('tree-list', {
   props: ['children', 'name', 'level', 'item'],
   data() {
-    return {
-
-    }
+    return {}
   },
   methods: {
     toggle(container, event) {
-      const chevron = event.currentTarget;
-      const containerItem = document.querySelector(`.${container}`);
-      const itemsToToggle = containerItem.querySelectorAll("ul");
+      const chevron = event.currentTarget
+      const containerItem = document.querySelector(`.${container}`)
+      const itemsToToggle = containerItem.querySelectorAll('ul')
       itemsToToggle.forEach((itemToToggle) => {
-        itemToToggle.classList.toggle("hidden")
-      });
+        itemToToggle.classList.toggle('hidden')
+      })
 
-      if (chevron.classList.toggle("open")) {
-
-      };
+      if (chevron.classList.toggle('open')) {
+      }
     },
     getParentList(startingNode) {
-      let parentList = startingNode.parentNode;
+      let parentList = startingNode.parentNode
       while (!(parentList instanceof HTMLUListElement)) {
-        parentList = parentList.parentNode;
+        parentList = parentList.parentNode
       }
-      return parentList;
+      return parentList
     },
     toggleVisibility(item, children, event) {
-      console.log(item.isVisible(), item.shouldBeVisible);
-      let visibility = item.shouldBeVisible;
-      let nextVisibleState = !visibility;
-      item.shouldBeVisible = nextVisibleState;
-      const element = event.currentTarget;
-      element.classList.toggle('visible');
-      const tree = element.nextSibling.nextSibling;
+      item.shouldBeVisible = !item.shouldBeVisible
+      const element = event.currentTarget
+      element.classList.toggle('visible')
+      element.classList.toggle('invisible')
 
-      let parentList = this.getParentList(element);
-
-      let parentItem = item.getOwner();
-
-      this.visible = nextVisibleState;
-
-      /* if (nextVisibleState == true && parentItem && !parentItem.isVisible()) {
-         console.log('parent is still not visible');
-         nextVisibleState = true;
-       }*/
-      const childrenToKeepHidden = [];
-      if (nextVisibleState && children) {
+      const childrenToKeepHidden = []
+      if (item.shouldBeVisible && children) {
         children.forEach((child) => {
-          console.log(child.name, child.item.isVisible());
           if (!child.item.isVisible() && !child.item.shouldBeVisible) {
-            childrenToKeepHidden.push(child);
-          };
-
+            childrenToKeepHidden.push(child)
+          }
         })
-      };
-      item.setVisible(nextVisibleState);
+      }
 
+      item.setVisible(item.shouldBeVisible)
 
-      childrenToKeepHidden.forEach(child => {
-        child.item.setVisible(false);
-      });
+      childrenToKeepHidden.forEach((child) => {
+        child.item.setVisible(false)
+      })
 
-      /* if (parentList) {
-         parentList.querySelectorAll("span").forEach((span, index) => {
-           if (index > 0) {
-             if (visibility) {
-               span.classList.add('parentHidden');
-               if (children) {
-                 children.forEach((child) => {
-                   this.toggleVisibility(child.item, child.children, event);
-                 })
-               };
-             } else {
-               span.classList.remove('parentHidden');
-               if (children) {
-                 children.forEach((child) => {
-                   this.toggleVisibility(child.item, child.children, event);
-                 })
-               };
-             }
-           };
-         });
-       };*/
-
-
-      //  console.log(nextVisibleState);
-
-
-    }
+      let parentList = this.getParentList(element)
+      const allChildElements = parentList.querySelectorAll('span')
+      allChildElements.forEach((span, index) => {
+        if (index > 0) {
+          if (!item.isVisible()) {
+            span.classList.add('parentHidden')
+          } else {
+            span.classList.remove('parentHidden')
+          }
+        }
+      })
+      const allInvisible = parentList.querySelectorAll('span.invisible')
+      allInvisible.forEach((span) => {
+        const spanParent = this.getParentList(span)
+        spanParent.querySelectorAll('span').forEach((spanTohide, index) => {
+          if (index > 0) {
+            spanTohide.classList.add('parentHidden')
+          }
+        })
+      })
+    },
   },
   template: `
   <ul :class='"list_"+level' class='tree-list'>
@@ -112,8 +90,7 @@ leftControls.component('tree-list', {
       <tree-list :level='parseInt(level) + 1 + "i" + index' v-for='(child, index) in children' :children='child.children' :item='child.item' :name='child.name' :key='index'></tree-list>
     </li>
   </ul>
-  `
+  `,
 })
 
-
-const leftControlsApp = leftControls.mount('#left-controls');
+const leftControlsApp = leftControls.mount('#left-controls')
